c5c5f21ccd80c9c24469aa9f69a4364d1bc9ccc1
xmiwon <procrutivate@outlook.com>
2021-01-05 16:13:23 +0100

    Recreating project in 2021

---
+++ src/components/SearchBox.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const SearchBox = ({searchfield, searchChange}) => {
+    return (
+        <div>
+            <input className="pa3 ba b--green bg-lightest-blue" type="search" placeholder="search robots" onChange={searchChange} />
+        </div>
+    )
+}
+
+export default SearchBox;
---
+++ src/containers/App.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react'
+import CardList from '../components/CardList'
+import SearchBox from '../components/SearchBox'
+import Scroll from '../components/Scroll'
+import ErrorBoundry from '../components/ErrorBoundry'
+
+
+const App = () => {
+    const [robots, setRobots] = useState([])
+    const [searchfield, setSearchfield] = useState('')
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => setRobots(users))
+    }, []) 
+
+    const onSearchChange = (event) => setSearchfield(event.target.value)
+    
+
+  
+       
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+    })
+
+        return !robots.length ?
+            <p>Loading..</p> :
+            (
+                <div className="tc">
+                    <h1>RoboFriends</h1>
+                    <SearchBox searchChange={onSearchChange} searchfield={searchfield} />
+                    <Scroll>
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBoundry>
+                    </Scroll>
+                </div>
+            )
+    
+        
+        
+
+        
+    
+    
+}
+
+export default App
